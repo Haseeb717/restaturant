37a87af4ca0ca2f9aa2bba46fbb68f5830f9a319
haseeb <haseeb@yallamotor.com>
2018-05-20 05:42:17 +0500

    Add to cart

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,29 +3,49 @@ function myFunction(id){
   radio_button = $("#modal_"+id+" input:checked");
   menu_item_name = $("#menu_item_name_"+id).text();
   menu_item_price = $(".menu_item_price_"+id).text();
-  quantity = $(".quantity").val();
-  description = $(".comment").val();
+  quantity = $(".quantity"+id).val();
+  description = $(".comment"+id).val();
   span = "<span class='block'></span> ";
 
   if(radio_button.length == 0){
-  	row = "<tr><td>" + quantity.toString() + "x " + menu_item_name +  span + "</td><td>" + quantity*menu_item_price + "</td></tr>";
+
+  	if(description.length !=0){
+			row = "<tr><td>" + quantity.toString() + "x " + menu_item_name +  span + "<br><span class='new_line'> + "+ description +"</span></td><td>" + quantity*menu_item_price + "</td></tr>";
+		}
+		else{
+			row = "<tr><td>" + quantity.toString() + "x " + menu_item_name +  span + "</td><td>" + quantity*menu_item_price + "</td></tr>";
+		}
+
+
   	$(".table_order").append(row);
   }
   else{
 		$("#modal_"+id+" input:checked").each(function() {
-			sides_item_id = $(this).attr('value');
+			selected.push($(this).attr('value'));
+		});
+
+		column = "<td>"+ quantity.toString() + "x " + menu_item_name;
+		$.each(selected,function(index,value){
+			sides_item_id = value;
 		  sides_item_name = $(".side_item_name_"+sides_item_id).text();
 		  sides_item_price = $(".side_item_price_"+sides_item_id).text();
-		  
-		  if(sides_item_price.length == 0) {
-		  	row = "<tr><td>" + quantity.toString() + "x " + menu_item_name +  span + "+ " + sides_item_name + "</td><td>" + quantity*menu_item_price + "</td></tr>";
+		  if(sides_item_price.length != 0) {
+		  	menu_item_price = sides_item_price;
 		  }
-		  else{
-		  	row = "<tr><td>" + quantity.toString() + "x " + menu_item_name +  span + "+ " + sides_item_name + "</td><td>" + quantity*sides_item_price + "</td></tr>";
 
-		  }
-		  $(".table_order").append(row);
+		  column += "<br><span class='new_line'> + "+ sides_item_name +"</span>";
+		  
 		});
+		if(description.length !=0){
+			column += "<br><span class='new_line'> + "+ description +"</span></td>";
+		}
+		else{
+			column += "</td>";
+		}
+		
+  	row = "<tr>" + column + "<td>" + quantity*menu_item_price + "</td></tr>";
+  
+	  $(".table_order").append(row);
 	}
 	$('#myModal'+id).modal('hide');
 }
