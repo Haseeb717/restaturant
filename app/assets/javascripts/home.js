3a232844dde13c25ac84d8b49481171cf650397a
haseeb <haseeb@yallamotor.com>
2018-07-01 12:55:02 +0500

    UI changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,9 +1,11 @@
 function myFunction(id){
   var selected = [];
+  total_price = 0;
   radio_button = $("#modal_"+id+" input:checked");
   menu_item_name = $("#menu_item_name_"+id).text();
   menu_item_price = $(".menu_item_price_"+id).text();
   quantity = $(".quantity"+id).val();
+
   description = $(".comment"+id).val();
   span = "<span class='block'></span> ";
 
@@ -16,7 +18,7 @@ function myFunction(id){
 			row = "<tr><td>" + quantity.toString() + " x " + menu_item_name +  span + "</td><td>" + quantity*menu_item_price + "</td></tr>";
 		}
 
-
+		total_price = total_price + (quantity*menu_item_price);
   	$(".table_order").append(row);
   }
   else{
@@ -29,13 +31,19 @@ function myFunction(id){
 			sides_item_id = value;
 		  sides_item_name = $(".side_item_name_"+sides_item_id).text();
 		  sides_item_price = $(".side_item_price_"+sides_item_id).text();
-		  if(sides_item_price.length != 0) {
-		  	menu_item_price = sides_item_price;
-		  }
+		  sides_item_type = $(".side_item_type_"+sides_item_id).text();
 
+		  if(sides_item_price.length != 0) {
+		  	if (side_item_type == "Extra"){
+		  		menu_item_price = (quantity*sides_item_price)+menu_item_price;
+		  	}
+		  	else{
+			  	menu_item_price = sides_item_price;		  		
+		  	}
+		  }
 		  column += "<br><span class='new_line'> + "+ sides_item_name +"</span>";
-		  
 		});
+
 		if(description.length !=0){
 			column += "<br><span class='new_line'> + "+ description +"</span></td>";
 		}
@@ -44,8 +52,13 @@ function myFunction(id){
 		}
 		
   	row = "<tr>" + column + "<td>" + quantity*menu_item_price + "</td></tr>";
-  
+  	total_price = total_price + (quantity*menu_item_price);
+
 	  $(".table_order").append(row);
 	}
+	existing_total = parseFloat($("#total").text());
+	total_price = total_price + existing_total;
+	$("#total").text(total_price);
 	$('#myModal'+id).modal('hide');
+	total_price = 0;
 }
