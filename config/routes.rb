5bfa4c8c68b86a3f1beff0c6fed1c4e416ac1188
haseeb <haseeb@yallamotor.com>
2018-07-02 08:21:28 +0500

    Stripe integration

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   root 'home#index'
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
